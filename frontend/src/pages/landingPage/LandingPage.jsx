ac3eba3dc7b6f8c4af9d28feb17d2a03b6cb0c94
RHMhasini <hasini.rhm@gmail.com>
2026-04-24 19:06:39 +0530

    fixed issue vith nav bar positioning in ticketing page

--- frontend/src/pages/landingPage/LandingPage.jsx
+++ frontend/src/pages/landingPage/LandingPage.jsx
@@ -1,16 +1,22 @@
+import Navbar from "../../components/common/Navbar/Navbar";
 import Hero from "../../components/landingPage/Hero/Hero";
 import Features from "../../components/landingPage/Features/Features";
 import HowItWorks from "../../components/landingPage/HowItWorks/HowItWorks";
 import CtaBanner from "../../components/landingPage/CtaBanner/CtaBanner";
+import Footer from "../../components/common/Footer/Footer";
 
-function LandingPage() {
+function LandingPage({ theme, toggleTheme }) {
   return (
-    <main>
-      <Hero />
-      <Features />
-      <HowItWorks />
-      <CtaBanner />
-    </main>
+    <div>
+      <Navbar theme={theme} toggleTheme={toggleTheme} />
+      <main>
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <CtaBanner />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
